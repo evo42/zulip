5f67bc505246057692f0ab46103e257fa3d12d53
Rene Kapusta <rene@evo42.local>
2019-09-13 22:22:53 +0200

    js fix guests without pm list

--- static/js/ui.js
+++ static/js/ui.js
@@ -12,7 +12,11 @@ exports.replace_emoji_with_text = function (element) {
 };
 
 exports.get_content_element = function (element_selector) {
-    const element = element_selector.expectOne()[0];
+    if (element_selector.length < 1) {
+        return; // BBS
+    }
+    
+    var element = element_selector.expectOne()[0];
     if (element.SimpleBar) {
         return $(element.SimpleBar.getContentElement());
     }

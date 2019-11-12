d571d1704edeeb033f962bbb62b40fc61075dcd9
Rene Kapusta <rene@evo42.net>
2019-11-12 16:06:48 +0100

    fix merge conflict

--- static/js/resize.js
+++ static/js/resize.js
@@ -220,11 +220,7 @@ exports.resize_stream_filters_container = function (h) {
 };
 
 exports.resize_page_components = function () {
-<<<<<<< HEAD
-    let sidebar;
-=======
     var sidebar;
->>>>>>> js: Convert vars declared separately and assigned once to const.
 
     return; // todo BBS
 
